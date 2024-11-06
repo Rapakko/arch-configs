5e9b605a5d5371f686471abc633841158e38f939
Rapakko <90797932+Rapakko@users.noreply.github.com>
2024-11-06 14:11:34 +0200

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -U fish_greeting
+    pfetch
 end
+
+alias rmf='rm -rf'
+alias ls='exa'
+alias lsa='exa -lah'
+alias btop='bashtop'
+alias cosmere='ssh -p 200 frans@192.168.1.253'
+alias hypexit='hyprctl dispatch exit'
+alias byebye='shutdown now'
+alias goodbye='shutdown now'
+
+# Created by `pipx` on 2024-06-23 21:14:38
+set PATH $PATH /home/freija/.local/bin
